ea58bdbb27696dd6cfb021161b3d53ccc022342e
Angel9921 <la.angarita@uniandes.edu.co>
2024-11-02 08:41:48 -0500

    duplicados

--- src/logica/coleccion.py
+++ src/logica/coleccion.py
@@ -29,6 +29,28 @@ class Coleccion():
     else:
         return False
 
+    def editar_album_duplicado(self, album_id, titulo, anio, descripcion, medio):
+        busqueda = session.query(Album).filter(Album.titulo == titulo, Album.id != album_id).all()
+        if len(busqueda) == 0:
+            album = session.query(Album).filter(Album.id == album_id).first()
+            album.titulo = titulo
+            album.ano = anio
+            album.descripcion = descripcion
+            album.medio = medio
+            session.commit()
+            return True
+        else:
+            return False
+
+    def eliminar_album_eliminar(self, album_id):
+        try:
+            album = session.query(Album).filter(Album.id == album_id).first()
+            session.delete(album)
+            session.commit()
+            return True
+        except:
+            return False
+
 
     def dar_medios(self):
         return [medio.name for medio in Medio]
